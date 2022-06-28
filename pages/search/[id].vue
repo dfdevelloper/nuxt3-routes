c456dcf92e5d76471f3dbac96f282e6c8184036a
Danillo Fidel <danillopkt@gmail.com>
2022-06-28 16:45:35 -0300

    update

--- pages/search/[id].vue
+++ pages/search/[id].vue
@@ -3,26 +3,40 @@
     <NuxtLink to="/"><button>Back</button></NuxtLink>
     <h3>List of Episodes</h3>
     <!-- {{ "p" + pending }} -->
-   <div style="display: flex; border: 1px solid red;">
-     <div class="shows" v-for="episode in fidel.slice(0, 3)" :key="episode.id">
-      <div style="display: flex; flex-direction: column; height: 200px;">
-        <span>{{ episode.name + " SERVER" }}</span>
-        <img :src="episode.image?.medium"  />
+    <div style="display: flex; border: 1px solid red">
+      <div class="shows" v-for="episode in fidel.slice(0, 3)" :key="episode.id">
+        <div style="display: flex; flex-direction: column; height: 200px">
+          <span>{{ episode.name + " SERVER" }}</span>
+          <img :src="episode.image?.medium" />
+        </div>
+      </div>
+
+      <div
+        class="shows"
+        v-for="episode in episodes.slice(3, 6)"
+        :key="episode.id"
+        style="display: flex; border: 1px solid blue"
+      >
+        <div style="display: flex; flex-direction: column; height: 200px">
+          <span>{{ episode.name + "from client" }}</span>
+          <img :src="episode.image?.medium" />
+        </div>
       </div>
     </div>
 
-    <div
-      class="shows"
-      v-for="episode in episodes.slice(3, 6)"
-      :key="episode.id"
-      style="display: flex; border: 1px solid blue;"
+    <video
+      v-for="n in 20"
+      :key="n"
+      width="320"
+      height="240"
+      autoplay
+      loop
+      preload="metadata"
+      muted
     >
-       <div style="display: flex; flex-direction: column; height: 200px;">
-        <span>{{ episode.name + "from client" }}</span>
-        <img :src="episode.image?.medium"  />
-      </div>
-    </div>
-   </div>
+      <source src="~/assets/sample-mp4-file.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   </div>
 </template>
 
