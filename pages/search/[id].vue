35916282bea97f4ced6f448ad1dffe61ed9313fd
Danillo Fidel <danillopkt@gmail.com>
2022-06-28 11:29:57 -0300

    update

--- pages/search/[id].vue
+++ pages/search/[id].vue
@@ -2,12 +2,27 @@
   <div>
     <NuxtLink to="/"><button>Back</button></NuxtLink>
     <h3>List of Episodes</h3>
-
-    <div class="shows" v-for="episode in episodes" :key="episode.id">
-      <div>{{ episode.name }}</div>
-      Season: {{ episode.season }} Ep: {{ episode.number }}
-      <img :src="episode.image?.medium" />
+    <!-- {{ "p" + pending }} -->
+   <div style="display: flex; border: 1px solid red;">
+     <div class="shows" v-for="episode in fidel.slice(0, 3)" :key="episode.id">
+      <div style="display: flex; flex-direction: column; height: 200px;">
+        <span>{{ episode.name + " SERVER" }}</span>
+        <img :src="episode.image?.medium"  />
+      </div>
     </div>
+
+    <div
+      class="shows"
+      v-for="episode in episodes.slice(3, 6)"
+      :key="episode.id"
+      style="display: flex; border: 1px solid blue;"
+    >
+       <div style="display: flex; flex-direction: column; height: 200px;">
+        <span>{{ episode.name + "from client" }}</span>
+        <img :src="episode.image?.medium"  />
+      </div>
+    </div>
+   </div>
   </div>
 </template>
 
@@ -15,7 +30,18 @@
 import { ref, onMounted } from "vue";
 
 const route = useRoute();
+
 const episodes = ref([]);
+const fidel = ref([]);
+
+const abc = await fetch(
+  `https://api.tvmaze.com/shows/${route.params.id}?embed=episodes`
+);
+
+const json = await abc.json();
+fidel.value = json._embedded.episodes;
+
+// episodes.value = data._embedded.episodes;
 
 onMounted(async () => {
   const fetchData = await fetch(
